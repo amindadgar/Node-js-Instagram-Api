b191f15919cdef6487a61c1fc15ea5293c1e97c8
Amin <dadgaramin96@gmail.com>
2020-12-30 20:22:15 +0330

    explore-users api updated!

--- routes/explore/explore-users-route.js
+++ routes/explore/explore-users-route.js
@@ -11,7 +11,19 @@ app.get('/users/:id',(req,res) =>{
         connection.query('CALL GetUser(?);',[req.params.id],(err, rows) =>{
             if(err) throw err;
             else {
-                 res.send(rows[0]);
+                if(rows[0].length != 0){
+                    const sendobject = {
+                        availableConversation: true,
+                        data:rows[0]
+                    };
+                    res.send(sendobject);
+                }else{
+                    const sendobject = {
+                        availableConversation: false,
+                        data:{}
+                    };
+                    res.send(sendobject);
+                }
             }
         });
 });
