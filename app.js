5a973b7751c7849fea9e7d60f0e383624bf0dc85
Amin <dadgaramin96@gmail.com>
2020-12-30 13:02:07 +0330

    database connection is now shared between files

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // import the libraries
-const mysql = require('mysql');
 const express = require('express');
 require('dotenv').config();
 // initialize express framework
@@ -12,40 +11,5 @@ app.listen(process.env.PORT,() =>{
     console.log(`server is running on port ${process.env.PORT}`)
 })
 
-
-// create database connection
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user : process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-connection.connect((err)=> {
-    if(err)
-        throw err;
-    else
-        console.log('database connected!');
-});
-
-
-// example url:  /users?id=1
-app.get('/users',(req,res) =>{
-    const id = req.query.id;
-    if(id != undefined){
-        connection.query('CALL GetUser(?);',id,(err, rows) =>{
-            if(err) throw err;
-            else {
-                 res.send(rows[0]);
-            }
-        });
-    }else {
-        connection.query('select * from users',(err,rows) =>{
-            if(err);
-            else
-                res.send(rows);
-        });
-    }
-});
 
 AppRoutes(app);
---
+++ db-connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+
+// create database connection
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user : process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err)=> {
+    if(err)
+        throw err;
+    else
+        console.log('database connected!');
+});
+
+module.exports = connection;
