e9ac02e74d5ed84c197723404e86a01ef578aaff
Amin <dadgaramin96@gmail.com>
2020-12-31 16:21:40 +0330

    changes made for signup due to avoid sql injection

--- db-connection.js
+++ db-connection.js
@@ -7,8 +7,7 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    multipleStatements: true
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
--- routes/signup-route.js
+++ routes/signup-route.js
@@ -19,19 +19,20 @@ app.post('/signup', express.json(), async (req, res) => {
         const hashedPassword = await bcrypt.hash(emp.password, 10);
 
         // the sql query needed for calling procedure
-        const query = 'SET @UserName = ?; SET @firstname = ?; \
-        SET @surname = ?; SET @email = ?; SET @password = ?; \
-        SET @bio = ?; SET @joined = ?; SET @email_verified = ?; \
-        SET @account_type = ?; SET @instagram = ?; SET @twitter = ?; SET @facebook = ?; \
-        SET @github = ?; SET @website = ?; SET @phone = ?; SET @isOnline = ?; SET @lastOnline = ?; \
-        CALL AddUser(@UserName,@firstname,@surname \
-            ,@email,@password,@bio,@joined \
-            ,@email_verified ,@account_type, \
-            @instagram,@twitter,@facebook,@github, \
-            @website,@phone,@isOnline,@lastOnline );';
 
-        connection.query(query, [emp.username, emp.firstname, emp.surname, emp.email, hashedPassword, emp.bio, emp.joined, emp.email_verified,
-            emp.accounttype, emp.instagram, emp.twitter, emp.facebook, emp.gitHub, emp.website, emp.phone, emp.isonline, emp.lastonline
+        // question marks values are as this comments below
+        // username,firstname, surname, 
+        // email, password, bio,joined
+        // email_verified, account_type
+        // instagram, twitter, facebook, gitHub, website, phone, isOnline,lashOnline
+        const newQuery = 'CALL AddUser(? , ?, ? \
+            ,?, ?, ?, ? \
+            ,? , ?, \
+            ?, ?,? ,?, \
+            ?, ?, ?, ? );'
+
+        connection.query(newQuery, [emp.username, emp.firstname, emp.surname, emp.email, hashedPassword, emp.bio, emp.joined, emp.email_verified,
+            emp.account_type, emp.instagram, emp.twitter, emp.facebook, emp.gitHub, emp.website, emp.phone, emp.isonline, emp.lastonline
         ], (err, rows) => {
             if (err) throw err;
             else {
